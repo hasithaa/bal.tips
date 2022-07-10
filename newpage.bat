358d15484715a87fc696b5e7851377c7bf92ca1d
Hasitha Aravinda <hasithaa@users.noreply.github.com>
2022-07-10 09:18:33 +0530

    Update new commands

--- newpage.bat
+++ newpage.bat
@@ -1,1 +1,6 @@
-hugo new --kind page %1
+@echo off
+setlocal enableextensions disabledelayedexpansion
+
+for /f "tokens=2,* delims=\" %%a in ("%1") do set "page=%%b"
+
+@REM hugo new --kind page %page%
--- newtip.bat
+++ newtip.bat
@@ -1,1 +1,6 @@
-hugo new --kind tip examples/%1
+@echo off
+setlocal enableextensions disabledelayedexpansion
+
+for /f "tokens=3,* delims=\" %%a in ("%1") do set "tip=%%b"
+
+hugo new --kind tip examples/%tip%
