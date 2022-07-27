bb419382bcbed629f8e616d19be7841974d6ebb8
Hasitha Aravinda <hasithaa@users.noreply.github.com>
2022-07-27 08:54:38 +0530

    Remove images

--- newpage.bat
+++ newpage.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enableextensions disabledelayedexpansion
 
-for /f "tokens=2,* delims=\" %%a in ("%1") do set "page=%%b"
+for /f "tokens=1,* delims=\" %%a in ("%1") do set "page=%%b"
 
 hugo new --kind page %page%
