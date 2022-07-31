ce4702660547fc933ffdf58186dd4adf4d4ab559
Hasitha Aravinda <hasithaa@users.noreply.github.com>
2022-07-31 22:21:22 +0530

    Add README.md

--- newtip.bat
+++ newtip.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enableextensions disabledelayedexpansion
 
-for /f "tokens=2,* delims=\" %%a in ("%1") do set "tip=%%b"
+for /f "tokens=3,* delims=\" %%a in ("%1") do set "tip=%%b"
 
 hugo new --kind tip examples/%tip%
