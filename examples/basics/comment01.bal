ef13824c8786f61a21a22ea38bf3b3337dc97ef6
hasithaa <hasithaa@users.noreply.github.com>
2022-11-17 04:04:25 +0000

    deploy: 75de310386e0c5279d38ffd4cb297b5821265955

---
+++ examples/basics/comment01.bal
@@ -0,0 +1,11 @@
+// Hello world Ballerina Program.
+import ballerina/io;
+//tip-start
+// My Main function.
+public function main() {
+  io:println("Hello World...!!!"); // printing hello world.
+
+  //io:println("Comment");
+}
+//tip-end
+// End of this example.
--- js/docdock.js
+++ js/docdock.js
@@ -157,43 +157,43 @@ jQuery(document).ready(function() {
 
     // clipboard
     var clipInit = false;
-    $('code').each(function() {
-        var code = $(this),
-            text = code.text();
+    // $('code').each(function() {
+    //     var code = $(this),
+    //         text = code.text();
 
-        if (text.length > 20 && !text.startsWith(" 1\n 2\n")) {
-            if (!clipInit) {
-                var text, clip = new Clipboard('.copy-to-clipboard', {
-                    text: function(trigger) {
-                        text = $(trigger).prev('code').text();
-                        return text.replace(/^\$\s/gm, '');
-                    }
-                });
+    //     if (text.length > 40 && !text.startsWith(" 1\n 2\n")) {
+    //         if (!clipInit) {
+    //             var text, clip = new Clipboard('.copy-to-clipboard', {
+    //                 text: function(trigger) {
+    //                     text = $(trigger).prev('code').text();
+    //                     return text.replace(/^\$\s/gm, '');
+    //                 }
+    //             });
 
-                var inPre;
-                clip.on('success', function(e) {
-                    e.clearSelection();
-                    inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
-                    $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-                });
+    //             var inPre;
+    //             clip.on('success', function(e) {
+    //                 e.clearSelection();
+    //                 inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
+    //                 $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
+    //             });
 
-                clip.on('error', function(e) {
-                    inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
-                    $(e.trigger).attr('aria-label', fallbackMessage(e.action)).addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-                    $(document).one('copy', function(){
-                        $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
-                    });
-                });
+    //             clip.on('error', function(e) {
+    //                 inPre = $(e.trigger).parent().prop('tagName') == 'PRE';
+    //                 $(e.trigger).attr('aria-label', fallbackMessage(e.action)).addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
+    //                 $(document).one('copy', function(){
+    //                     $(e.trigger).attr('aria-label', 'Copied to clipboard!').addClass('tooltipped tooltipped-' + (inPre ? 'w' : 's'));
+    //                 });
+    //             });
 
-                clipInit = true;
-            }
+    //             clipInit = true;
+    //         }
 
-            code.after('<span class="copy-to-clipboard" title="Copy to clipboard"><object class="clippy-icon" type="image/svg+xml" data="' + baseurl + 'images/clippy.svg"/></span>');
-            code.next('.copy-to-clipboard').on('mouseleave', function() {
-                $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
-            });
-        }
-    });
+    //         code.after('<span class="copy-to-clipboard" title="Copy to clipboard"><object class="clippy-icon" type="image/svg+xml" data="' + baseurl + 'images/clippy.svg"/></span>');
+    //         code.next('.copy-to-clipboard').on('mouseleave', function() {
+    //             $(this).attr('aria-label', null).removeClass('tooltipped tooltipped-s tooltipped-w');
+    //         });
+    //     }
+    // });
 
     // allow keyboard control for prev/next links
     jQuery(function() {
